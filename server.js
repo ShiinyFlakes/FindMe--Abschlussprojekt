4787402ddbb9d7f8016ea198560accbde7b5ff5d
unknown <mago_elm@hotmail.com>
2022-06-13 14:44:06 +0200

    routes & deps

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { appendFile } = require("fs");
+const mongoose = require("mongoose");
+
+const PORT = process.env || 8000 ;
+
+
+app.get("/", (req, res)),
+app.put();
+app.post();
+app.delete();
