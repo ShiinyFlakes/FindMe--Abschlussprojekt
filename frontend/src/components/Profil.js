f7d3ce6fd8f8c7123d7b7ecce4526c8aba2331cb
Mak513 <osei.makangu@hotmail.com>
2022-06-22 15:34:15 +0200

    Profil basic (Form fehlt)

--- frontend/src/components/Profil.js
+++ frontend/src/components/Profil.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Profil() {
+  const [details, setDetails] = useState({});
+
+  const loadData = async () => {
+    try {
+      const response = await axios.get();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/profil`);
+        setDetails(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadData();
+  });
+
   return (
     <div>
       <h1>Profil</h1>
+      <></>
     </div>
   );
 }
